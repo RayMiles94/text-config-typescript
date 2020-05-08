162a8ae6c8f8e00435bd9cca5ed1e238600875f4
raouf-slnee <raouf.jabri@slnee.com>
2020-05-08 14:23:19 +0100

    nodemon

--- src/type.ts
+++ src/type.ts
@@ -3,6 +3,7 @@ export class TxtFileType {
     constructor(public filename: string) {}
 
     public checkfile(): boolean {
+        // create type
         const type = this.filename.split('.');
         if (type[type.length-1]=='txt'){
             return true;
